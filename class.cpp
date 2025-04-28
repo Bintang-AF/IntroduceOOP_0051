f52d52cd37aabf471aefab64f9d2d8a1b5019e88
Bintang-AF <bintang.aulia.ft24@mail.umy.ac.id>
2025-04-28 11:06:56 +0700

    prosedur input data

--- class.cpp
+++ class.cpp
@@ -9,4 +9,15 @@ public:
 
     private:
     int harga_mobil;
+    public:
+    void input_data(){
+        cout << "nama mobil = ";
+        cin >> Nama_Mobil;
+        cout << "masukkan tipe mobil = ";
+        cin >> Tipe_mobil;
+        cout << "masukkan harga mobil = ";
+        cin >> harga_mobil;
+
+    }
+
 };
